f1c889f58049a6402c759050ec4c4660a53c37ff
ZHI <891023883@qq.com>
2021-12-16 23:59:15 +0800

    swagger

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -2,11 +2,14 @@ package com.atguigu.yygh.hosp.controller;
 
 import com.atguigu.yygh.hosp.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = "医院设置管理")
 @RestController
 @RequestMapping("/admin/hosp/hospitalSet")
 public class HospitalSetController {
@@ -15,6 +18,7 @@ public class HospitalSetController {
     private HospitalSetService hospitalSetService;
 
     //1 查询医院设置表所有信息
+    @ApiOperation(value = "获取所有医院信息")
     @GetMapping("findAll")
     public List<HospitalSet> findAllHospitalSet() {
         // 调用service里的方法
@@ -23,8 +27,9 @@ public class HospitalSetController {
     }
 
     //2 删除医院设置
+    @ApiOperation(value = "逻辑删除医院信息")
     @DeleteMapping("{id}")
-    public boolean removeHospSet(@PathVariable Long id){
+    public boolean removeHospSet(@PathVariable Long id) {
         boolean flag = hospitalSetService.removeById(id);
         return flag;
     }
