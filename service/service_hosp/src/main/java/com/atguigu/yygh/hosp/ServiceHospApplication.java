cfafe5ee3a96e6f71fcac5c3083de7ba15b5916c
ZHI <891023883@qq.com>
2021-12-16 23:18:51 +0800

    医院设置查询模块测试

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -1,4 +1,4 @@
-package com.atguitu.yygh.hosp;
+package com.atguigu.yygh.hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
