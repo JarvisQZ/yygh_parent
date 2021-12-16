eaf586b646c159fe7396f88807f9cd323bfc41b7
ZHI <891023883@qq.com>
2021-12-17 00:17:31 +0800

    添加统一返回结果类

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -1,5 +1,6 @@
 package com.atguigu.yygh.hosp.controller;
 
+import com.atguigu.yygh.common.result.Result;
 import com.atguigu.yygh.hosp.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
 import io.swagger.annotations.Api;
@@ -20,17 +21,21 @@ public class HospitalSetController {
     //1 查询医院设置表所有信息
     @ApiOperation(value = "获取所有医院信息")
     @GetMapping("findAll")
-    public List<HospitalSet> findAllHospitalSet() {
+    public Result findAllHospitalSet() {
         // 调用service里的方法
         List<HospitalSet> list = hospitalSetService.list();
-        return list;
+        return Result.ok(list);
     }
 
     //2 删除医院设置
     @ApiOperation(value = "逻辑删除医院信息")
     @DeleteMapping("{id}")
-    public boolean removeHospSet(@PathVariable Long id) {
+    public Result removeHospSet(@PathVariable Long id) {
         boolean flag = hospitalSetService.removeById(id);
-        return flag;
+        if (flag) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
     }
 }
