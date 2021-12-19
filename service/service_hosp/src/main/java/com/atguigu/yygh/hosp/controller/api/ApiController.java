72ffe7db875f9dae82cbdb736e47050411071ae5
ZHI <891023883@qq.com>
2021-12-20 01:44:12 +0800

    医院上传接口最终实现

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/api/ApiController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/api/ApiController.java
@@ -1,8 +1,12 @@
 package com.atguigu.yygh.hosp.controller.api;
 
+import com.atguigu.yygh.common.exception.YyghException;
 import com.atguigu.yygh.common.helper.HttpRequestHelper;
 import com.atguigu.yygh.common.result.Result;
+import com.atguigu.yygh.common.result.ResultCodeEnum;
+import com.atguigu.yygh.common.utils.MD5;
 import com.atguigu.yygh.hosp.service.HospitalService;
+import com.atguigu.yygh.hosp.service.HospitalSetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +22,35 @@ public class ApiController {
     @Autowired
     private HospitalService hospitalService;
 
+    @Autowired
+    private HospitalSetService hospitalSetService;
+
     //上传医院接口
     @PostMapping("/saveHospital")
-    public Result saveHosp(HttpServletRequest request){
+    public Result saveHosp(HttpServletRequest request) {
         //获取传递过来的医院信息
         Map<String, String[]> requestMap = request.getParameterMap();
         Map<String, Object> paramMap = HttpRequestHelper.switchMap(requestMap);
+
+        //1 获取医院系统传递过来的签名, MD5加密
+        String hospSign = (String) paramMap.get("sign");
+
+        //2 根据传递过来的医院编号，查询数据库，查询签名
+        String hoscode = (String) paramMap.get("hoscode");
+        String signKey = hospitalSetService.getSignKey(hoscode);
+
+        //3 查询出的签名进行MD5加密
+        String signKeyMD5 = MD5.encrypt(signKey);
+
+        //4 判断签名是否一致
+        if (!hospSign.equals(signKeyMD5)) {
+            throw new YyghException(ResultCodeEnum.SIGN_ERROR);
+        }
+
+        //传输过程中”+“转换为了” “，因此我们要转换回来
+        String logoData = (String) paramMap.get("logoData");
+        logoData = logoData.replaceAll(" ", "+");
+        paramMap.put("logoData", logoData);
 
         //调用service方法
         hospitalService.save(paramMap);
