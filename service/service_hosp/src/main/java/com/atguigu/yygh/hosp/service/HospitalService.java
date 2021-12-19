bd439ccc38deb36796a408b1fb7ac1a18d4416f7
ZHI <891023883@qq.com>
2021-12-20 00:30:06 +0800

    医院上传接口基本实现

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/service/HospitalService.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/service/HospitalService.java
@@ -1,4 +1,7 @@
 package com.atguigu.yygh.hosp.service;
 
+import java.util.Map;
+
 public interface HospitalService {
+    void save(Map<String, Object> paramMap);
 }
--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/service/impl/HospitalServiceImpl.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/service/impl/HospitalServiceImpl.java
@@ -1,9 +1,14 @@
 package com.atguigu.yygh.hosp.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.atguigu.yygh.hosp.repository.HospitalRepository;
 import com.atguigu.yygh.hosp.service.HospitalService;
+import com.atguigu.yygh.model.hosp.Hospital;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Map;
 
 @Service
 public class HospitalServiceImpl implements HospitalService {
@@ -11,5 +16,32 @@ public class HospitalServiceImpl implements HospitalService {
     @Autowired
     private HospitalRepository hospitalRepository;
 
+    //添加数据
+    @Override
+    public void save(Map<String, Object> paramMap) {
+        //把参数map集合转换成对象Hospital
+        String mapString = JSONObject.toJSONString(paramMap);
+        Hospital hospital = JSONObject.parseObject(mapString, Hospital.class);
 
+        // 判断是否存在相同数据
+        String hoscode = hospital.getHoscode();
+        Hospital hospitalExist = hospitalRepository.getHospitalByHoscode(hoscode);
+
+
+        if (hospitalExist != null){//存在，进行修改更新
+            hospital.setStatus(hospitalExist.getStatus());
+            hospital.setCreateTime(hospitalExist.getCreateTime());
+            hospital.setUpdateTime(new Date());
+            hospital.setIsDeleted(0);
+            hospitalRepository.save(hospital);
+        }else {//不存在，进行添加
+            hospital.setStatus(0);
+            hospital.setCreateTime(new Date());
+            hospital.setUpdateTime(new Date());
+            hospital.setIsDeleted(0);
+            hospitalRepository.save(hospital);
+        }
+
+
+    }
 }
