70db6e036b8e3c13df4d1bb5856e334bee83d84f
ZHI <891023883@qq.com>
2021-12-21 21:26:04 +0800

    科室列表

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/service/DepartmentService.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/service/DepartmentService.java
@@ -2,9 +2,11 @@ package com.atguigu.yygh.hosp.service;
 
 import com.atguigu.yygh.model.hosp.Department;
 import com.atguigu.yygh.vo.hosp.DepartmentQueryVo;
+import com.atguigu.yygh.vo.hosp.DepartmentVo;
 import org.springframework.data.domain.Page;
 
 
+import java.util.List;
 import java.util.Map;
 
 public interface DepartmentService {
@@ -14,4 +16,7 @@ public interface DepartmentService {
     Page<Department> findPageDepartment(int page, int limit, DepartmentQueryVo departmentQueryVo);
     // 删除科室接口
     void remove(String hoscode, String depcode);
+
+    //根据医院编号，查询所有科室列表
+    List<DepartmentVo> findDeptTree(String hoscode);
 }
--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/service/impl/DepartmentServiceImpl.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/service/impl/DepartmentServiceImpl.java
@@ -5,13 +5,17 @@ import com.atguigu.yygh.hosp.repository.DepartmentRepository;
 import com.atguigu.yygh.hosp.service.DepartmentService;
 import com.atguigu.yygh.model.hosp.Department;
 import com.atguigu.yygh.vo.hosp.DepartmentQueryVo;
+import com.atguigu.yygh.vo.hosp.DepartmentVo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService {
@@ -71,5 +75,48 @@ public class DepartmentServiceImpl implements DepartmentService {
             //调用方法删除
             departmentRepository.deleteById(departmentExist.getId());
         }
+    }
+
+    //根据医院编号，查询所有科室列表
+    @Override
+    public List<DepartmentVo> findDeptTree(String hoscode) {
+        //1 创建list集合，用于最终数据封装
+        ArrayList<DepartmentVo> result = new ArrayList<>();
+        //根据医院编号，查询所有科室信息
+        Department departmentQuery = new Department();
+        departmentQuery.setHoscode(hoscode);
+        Example<Department> example = Example.of(departmentQuery);
+        //所有科室信息
+        List<Department> departmentList = departmentRepository.findAll(example);
+
+        //根据大科室编号 bigcode 分组， 获取每个大科室里面下级子科室
+        Map<String, List<Department>> departmentMap =
+                departmentList.stream().collect(Collectors.groupingBy(Department::getBigcode));
+        //遍历得到的map
+        for (Map.Entry<String, List<Department>> entry : departmentMap.entrySet()){
+            //大科室编号
+            String bigcode = entry.getKey();
+            //大科室编号对应的全部数据
+            List<Department> departmentList1 = entry.getValue();
+
+            //封装大科室
+            DepartmentVo departmentVo = new DepartmentVo();
+            departmentVo.setDepcode(bigcode);
+            departmentVo.setDepname(departmentList1.get(0).getDepname());
+            //封装小科室
+            ArrayList<DepartmentVo> children = new ArrayList<>();
+            for (Department department : departmentList1) {
+                DepartmentVo departmentVo2 = new DepartmentVo();
+                departmentVo2.setDepcode(department.getDepcode());
+                departmentVo2.setDepname(department.getDepname());
+                //封装到集合中去
+                children.add(departmentVo2);
+            }
+            //把小科室的list集合放到大科室的children里面
+            departmentVo.setChildren(children);
+            //把数据放到result
+            result.add(departmentVo);
+        }
+        return result;
     }
 }
