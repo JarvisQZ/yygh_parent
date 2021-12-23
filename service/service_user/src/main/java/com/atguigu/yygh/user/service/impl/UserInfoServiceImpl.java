472106af9810fc4429a14bfd5d0c770333732f07
ZHI <891023883@qq.com>
2021-12-23 22:49:51 +0800

    就诊人管理接口

--- service/service_user/src/main/java/com/atguigu/yygh/user/service/impl/UserInfoServiceImpl.java
+++ service/service_user/src/main/java/com/atguigu/yygh/user/service/impl/UserInfoServiceImpl.java
@@ -24,41 +24,44 @@ public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> i
 
     @Autowired
     private RedisTemplate<String, String> redisTemplate;
+    @Autowired
+    private UserInfoMapper userInfoMapper;
 
     @Override
     public Map<String, Object> loginUser(LoginVo loginVo) {
-        //从loginVo获取输入的手机号和验证码
         String phone = loginVo.getPhone();
         String code = loginVo.getCode();
-        //判断手机号和验证码是否为空
-        if (StringUtils.isEmpty(phone) || StringUtils.isEmpty(code)) {
+
+//校验参数
+        if(StringUtils.isEmpty(phone) ||
+                StringUtils.isEmpty(code)) {
             throw new YyghException(ResultCodeEnum.PARAM_ERROR);
         }
 
-        //从redis中读取验证码
-        String redisCode = redisTemplate.opsForValue().get(phone);
-        if (!code.equals(redisCode)) {
+//校验校验验证码
+        String mobleCode = redisTemplate.opsForValue().get(phone);
+        if(!code.equals(mobleCode)) {
             throw new YyghException(ResultCodeEnum.CODE_ERROR);
         }
 
-        //绑定手机号码
+//绑定手机号码
         UserInfo userInfo = null;
-        if (!StringUtils.isEmpty(loginVo.getOpenid())) {
+        if(!StringUtils.isEmpty(loginVo.getOpenid())) {
             userInfo = this.getWxInfoOpenId(loginVo.getOpenid());
-            if (null != userInfo) {
+            if(null != userInfo) {
                 userInfo.setPhone(loginVo.getPhone());
                 this.updateById(userInfo);
             } else {
                 throw new YyghException(ResultCodeEnum.DATA_ERROR);
             }
         }
-        //如果userInfo为空，进行手机登录
-        if (null != userInfo) {
-            // 判断是否是第一次登录：根据手机号查询数据库，如果不存在相同的手机号，就是第一次登录
-            QueryWrapper<UserInfo> wrapper = new QueryWrapper<>();
-            wrapper.eq("phone", phone);
-            userInfo = baseMapper.selectOne(wrapper);
-            if (userInfo == null) {
+
+//userInfo=null 说明手机直接登录
+        if(null == userInfo) {
+            QueryWrapper<UserInfo> queryWrapper = new QueryWrapper<>();
+            queryWrapper.eq("phone", phone);
+            userInfo = userInfoMapper.selectOne(queryWrapper);
+            if(null == userInfo) {
                 userInfo = new UserInfo();
                 userInfo.setName("");
                 userInfo.setPhone(phone);
@@ -67,29 +70,31 @@ public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> i
             }
         }
 
-
-        if (userInfo.getStatus() == 0) {
+//校验是否被禁用
+        if(userInfo.getStatus() == 0) {
             throw new YyghException(ResultCodeEnum.LOGIN_DISABLED_ERROR);
         }
-        // 不是第一次登录，进行登录
-        // 返回登录信息
-        //返回登录用户名
-        //返回token信息
-        HashMap<String, Object> map = new HashMap<>();
+
+//记录登录
+//        UserLoginRecord userLoginRecord = new UserLoginRecord();
+//        userLoginRecord.setUserId(userInfo.getId());
+//        userLoginRecord.setIp(loginVo.getIp());
+//        userLoginRecordMapper.insert(userLoginRecord);
+
+//返回页面显示名称
+        Map<String, Object> map = new HashMap<>();
         String name = userInfo.getName();
-        if (StringUtils.isEmpty(name)) {
+        if(StringUtils.isEmpty(name)) {
             name = userInfo.getNickName();
         }
-        if (StringUtils.isEmpty(name)) {
+        if(StringUtils.isEmpty(name)) {
             name = userInfo.getPhone();
         }
         map.put("name", name);
-
-        //Jwt token生成
-        String token = JwtHelper.createToken(userInfo.getId(), userInfo.getName());
+        String token = JwtHelper.createToken(userInfo.getId(), name);
         map.put("token", token);
-
         return map;
+
     }
 
     @Override
