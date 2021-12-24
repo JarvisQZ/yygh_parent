cc8edfc53cb2be472224b2fc67201f34e9e711a2
ZHI <891023883@qq.com>
2021-12-24 15:17:51 +0800

    用户平台管理: 用户列表接口

--- service/service_user/src/main/java/com/atguigu/yygh/user/service/UserInfoService.java
+++ service/service_user/src/main/java/com/atguigu/yygh/user/service/UserInfoService.java
@@ -3,6 +3,9 @@ package com.atguigu.yygh.user.service;
 import com.atguigu.yygh.model.user.UserInfo;
 import com.atguigu.yygh.vo.user.LoginVo;
 import com.atguigu.yygh.vo.user.UserAuthVo;
+import com.atguigu.yygh.vo.user.UserInfoQueryVo;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 import java.util.Map;
@@ -10,8 +13,13 @@ import java.util.Map;
 public interface UserInfoService extends IService<UserInfo> {
     //用户手机号登录接口
     Map<String, Object> loginUser(LoginVo loginVo);
+
     //根据openid判断
     UserInfo getWxInfoOpenId(String openId);
+
     //用户认证
     void userAuth(Long userId, UserAuthVo userAuthVo);
+
+    //用户列表接口(条件查询带分页)
+    IPage<UserInfo> selectPage(Page<UserInfo> pageParam, UserInfoQueryVo userInfoQueryVo);
 }
--- service/service_user/src/main/java/com/atguigu/yygh/user/service/impl/UserInfoServiceImpl.java
+++ service/service_user/src/main/java/com/atguigu/yygh/user/service/impl/UserInfoServiceImpl.java
@@ -9,7 +9,10 @@ import com.atguigu.yygh.user.mapper.UserInfoMapper;
 import com.atguigu.yygh.user.service.UserInfoService;
 import com.atguigu.yygh.vo.user.LoginVo;
 import com.atguigu.yygh.vo.user.UserAuthVo;
+import com.atguigu.yygh.vo.user.UserInfoQueryVo;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -33,22 +36,22 @@ public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> i
         String code = loginVo.getCode();
 
 //校验参数
-        if(StringUtils.isEmpty(phone) ||
+        if (StringUtils.isEmpty(phone) ||
                 StringUtils.isEmpty(code)) {
             throw new YyghException(ResultCodeEnum.PARAM_ERROR);
         }
 
 //校验校验验证码
         String mobleCode = redisTemplate.opsForValue().get(phone);
-        if(!code.equals(mobleCode)) {
+        if (!code.equals(mobleCode)) {
             throw new YyghException(ResultCodeEnum.CODE_ERROR);
         }
 
 //绑定手机号码
         UserInfo userInfo = null;
-        if(!StringUtils.isEmpty(loginVo.getOpenid())) {
+        if (!StringUtils.isEmpty(loginVo.getOpenid())) {
             userInfo = this.getWxInfoOpenId(loginVo.getOpenid());
-            if(null != userInfo) {
+            if (null != userInfo) {
                 userInfo.setPhone(loginVo.getPhone());
                 this.updateById(userInfo);
             } else {
@@ -57,11 +60,11 @@ public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> i
         }
 
 //userInfo=null 说明手机直接登录
-        if(null == userInfo) {
+        if (null == userInfo) {
             QueryWrapper<UserInfo> queryWrapper = new QueryWrapper<>();
             queryWrapper.eq("phone", phone);
             userInfo = userInfoMapper.selectOne(queryWrapper);
-            if(null == userInfo) {
+            if (null == userInfo) {
                 userInfo = new UserInfo();
                 userInfo.setName("");
                 userInfo.setPhone(phone);
@@ -71,7 +74,7 @@ public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> i
         }
 
 //校验是否被禁用
-        if(userInfo.getStatus() == 0) {
+        if (userInfo.getStatus() == 0) {
             throw new YyghException(ResultCodeEnum.LOGIN_DISABLED_ERROR);
         }
 
@@ -84,10 +87,10 @@ public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> i
 //返回页面显示名称
         Map<String, Object> map = new HashMap<>();
         String name = userInfo.getName();
-        if(StringUtils.isEmpty(name)) {
+        if (StringUtils.isEmpty(name)) {
             name = userInfo.getNickName();
         }
-        if(StringUtils.isEmpty(name)) {
+        if (StringUtils.isEmpty(name)) {
             name = userInfo.getPhone();
         }
         map.put("name", name);
@@ -122,4 +125,50 @@ public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> i
         //进行信息更新
         baseMapper.updateById(userInfo);
     }
+
+    //用户列表接口(条件查询带分页)
+    @Override
+    public IPage<UserInfo> selectPage(Page<UserInfo> pageParam, UserInfoQueryVo userInfoQueryVo) {
+        //UserInfoQueryVo获取条件值
+        String name = userInfoQueryVo.getKeyword();//用户名称
+        Integer status = userInfoQueryVo.getStatus();//用户状态
+        Integer authStatus = userInfoQueryVo.getAuthStatus();//认证状态
+        String createTimeBegin = userInfoQueryVo.getCreateTimeBegin();//开始时间
+        String createTimeEnd = userInfoQueryVo.getCreateTimeEnd();//结束时间
+        //对条件值进行非空判断
+        QueryWrapper<UserInfo> wrapper = new QueryWrapper<>();
+        if (!StringUtils.isEmpty(name)) {
+            wrapper.like("name", name);
+        }
+        if (!StringUtils.isEmpty(status)) {
+            wrapper.like("status", status);
+        }
+        if (!StringUtils.isEmpty(authStatus)) {
+            wrapper.like("auth_status", authStatus);
+        }
+        if (!StringUtils.isEmpty(createTimeBegin)) {
+            wrapper.ge("create_time", createTimeBegin);
+        }
+        if (!StringUtils.isEmpty(createTimeEnd)) {
+            wrapper.le("create_time", createTimeEnd);
+        }
+
+        Page<UserInfo> pages = baseMapper.selectPage(pageParam, wrapper);
+
+        //编号变成对应的值
+        pages.getRecords().stream().forEach(this::packageUserInfo);
+
+
+        return pages;
+    }
+
+    private UserInfo packageUserInfo(UserInfo userInfo) {
+        //处理认证状态编码
+        userInfo.getParam().put("authStatusString", AuthStatusEnum.getStatusNameByStatus(userInfo.getAuthStatus()));
+        //处理用户状态
+        String statusString = userInfo.getStatus() == 0 ? "锁定" : "正常";
+        userInfo.getParam().put("statusString", statusString);
+        return userInfo;
+    }
+
 }
